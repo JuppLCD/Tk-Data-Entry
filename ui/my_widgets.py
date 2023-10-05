5add2ad5f0627a7ecdeb64b13f00f49cc9355c0a
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-10-05 20:34:48 -0300

    feat: :sparkles: Create MyTable widget

--- ui/my_widgets.py
+++ ui/my_widgets.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
 
-from tkinter import LabelFrame, Label, Entry, Spinbox, StringVar, Checkbutton
-from tkinter.ttk import Combobox
+from tkinter import Frame, Label, Entry, Spinbox, StringVar, Checkbutton
+from tkinter.ttk import Combobox, Treeview, Scrollbar
+
+# Tkinter constants
+from tkinter import END, CENTER, NS, E
 
 
 class MyInput(ABC):
@@ -36,7 +39,7 @@ class MyInput(ABC):
 
 
 class MyInputText(MyInput):
-    def __init__(self, master: LabelFrame, text: str, position=(0, 0)):
+    def __init__(self, master: Frame, text: str, position=(0, 0)):
         label_text = f'{text.strip().capitalize()}'
 
         row = position[0]
@@ -58,7 +61,7 @@ class MyInputText(MyInput):
 
 
 class MyInputNumber(MyInput):
-    def __init__(self, master: LabelFrame, text: str, position=(0, 0), rangeInput=(0, 'infinity')):
+    def __init__(self, master: Frame, text: str, position=(0, 0), rangeInput=(0, 'infinity')):
         label_text = f'{text.strip().capitalize()}'
 
         row = position[0]
@@ -85,7 +88,7 @@ class MyInputNumber(MyInput):
 
 
 class MyInputSelect(MyInput):
-    def __init__(self, master: LabelFrame, text: str, position=(0, 0), options=tuple()):
+    def __init__(self, master: Frame, text: str, position=(0, 0), options=tuple()):
         label_text = f'{text.strip().capitalize()}'
 
         row = position[0]
@@ -118,7 +121,7 @@ class MyInputSelect(MyInput):
 
 
 class MyInputCheckBox(MyInput):
-    def __init__(self, master: LabelFrame, text_check_box: str, label_text="", position=(0, 0), onvalue: bool | str = True, offvalue: bool | str = False):
+    def __init__(self, master: Frame, text_check_box: str, label_text="", position=(0, 0), onvalue: bool | str = True, offvalue: bool | str = False):
         row = position[0]
         column = position[1]
 
@@ -145,3 +148,79 @@ class MyInputCheckBox(MyInput):
         )
         self.input.config(font=('Arial', 12))
         self.input.grid(row=row, column=column, padx=10, pady=5)
+
+
+class MyTable():
+    def __init__(
+            self,
+            master: Frame,
+            cols=(0, 1, 2),
+            cols_text=("#0", "#1", "#2"),
+            cols_width=(100, 100, 100),
+            position=(0, 0),
+            columnspan=1,
+            sticky=NS+E,
+            ** kwargs
+    ):
+        """
+        cols[0] will be the identifier of the element in the table
+        """
+
+        self.table = Treeview(master, **kwargs, columns=cols, show="headings")
+
+        self.table.grid(
+            row=position[0],
+            column=position[1],
+            columnspan=columnspan,
+            sticky=sticky
+        )
+
+        for i, col in enumerate(cols):
+            self.table.heading(
+                col,
+                text=cols_text[i],
+                anchor=CENTER
+            )
+
+        for i, col in enumerate(cols):
+            self.table.column(
+                col,
+                width=cols_width[i]
+            )
+
+        # Añadiendo y configurando el scroll de la tabla
+        self.scroll = Scrollbar(
+            master,
+            orient='vertical',
+            command=self.table.yview
+        )
+
+        col_scroll = position[1] + columnspan - 1
+        self.scroll.grid(row=position[0], column=col_scroll, sticky='nse')
+
+        self.table.configure(yscrollcommand=self.scroll.set)
+
+    def clear_table(self):
+        # get_children() retorna una lista, e * al inicio es el spreed operator
+        self.table.delete(*self.table.get_children())
+
+    def insert_item(self, item):
+        self.table.insert('', END, text=item[0], values=item)
+
+    def insert_all_items(self, all_items: list):
+        for item in all_items:
+            self.insert_item(item)
+
+    def select_item(self) -> dict | None:
+        try:
+            return self.table.item(self.table.selection())
+        except IndexError:
+            return None
+
+    def get_item(self) -> tuple | None:
+        item = self.select_item()
+
+        if item != None:
+            item = item['values']
+
+        return item
